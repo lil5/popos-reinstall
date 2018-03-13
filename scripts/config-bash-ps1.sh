f120353742cc82516d2235819e2c0cf20c8de400
Lucian I. Last <lil5@disroot.org>
2018-03-13 23:07:54 +0100

    Adhere to editorconfig using tabs

--- scripts/config-bash-ps1.sh
+++ scripts/config-bash-ps1.sh
@@ -25,18 +25,18 @@ $file_contents = file_get_contents($path_to_file);
 $regex='/if \[ "\$color_prompt" = yes \]; then(.*\n)*(\s*PS1=(.*\n)*){2}unset color_prompt force_color_prompt/';
 $replace="if [ \"\$color_prompt\" = yes ]; then
 
-  parse_git_branch() {
-    git branch 2> /dev/null | sed -e \"/^[^*]/d\" -e \"s/* \(.*\)/ (\\\\1)/\"
-  }
+	parse_git_branch() {
+		git branch 2> /dev/null | sed -e \"/^[^*]/d\" -e \"s/* \(.*\)/ (\\\\1)/\"
+	}
 
-  if [[ \${EUID} == 0 ]] ; then # if root
-    PS1=\"\${debian_chroot:+(\$debian_chroot)}\[\\\\033[01;31m\]\h\[\\\\033[01;34m\] \w\[\\\\033[00m\]\\\\\$(parse_git_branch)\[\\\\033[00m\] \#\[\\\\033[01;31m\] \"
-  else
-    echo \$USER@\$HOSTNAME
-    PS1=\"\${debian_chroot:+(\$debian_chroot)}\[\\\\033[01;32m\]\W\[\\\\033[33m\]\\\\\$(parse_git_branch)\[\\\\033[01;32m\] \\\$\[\\\\033[00m\] \"
-  fi
+	if [[ \${EUID} == 0 ]] ; then # if root
+		PS1=\"\${debian_chroot:+(\$debian_chroot)}\[\\\\033[01;31m\]\h\[\\\\033[01;34m\] \w\[\\\\033[00m\]\\\\\$(parse_git_branch)\[\\\\033[00m\] \#\[\\\\033[01;31m\] \"
+	else
+		echo \$USER@\$HOSTNAME
+		PS1=\"\${debian_chroot:+(\$debian_chroot)}\[\\\\033[01;32m\]\W\[\\\\033[33m\]\\\\\$(parse_git_branch)\[\\\\033[01;32m\] \\\$\[\\\\033[00m\] \"
+	fi
 else
-  PS1=\"\${debian_chroot:+(\$debian_chroot)}\u@\h \W \\\$ \"
+	PS1=\"\${debian_chroot:+(\$debian_chroot)}\u@\h \W \\\$ \"
 fi
 
 
